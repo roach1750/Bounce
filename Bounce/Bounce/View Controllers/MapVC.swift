7ab1e874f8f5a39e53c08f1caef51cd6817e1ff7
Andrew Roach <roach1750@gmail.com>
2016-01-16 11:41:24 -0500

    added Mapview and Location Manager

--- Bounce/Bounce/View Controllers/MapVC.swift
+++ Bounce/Bounce/View Controllers/MapVC.swift
@@ -7,29 +7,61 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapVC: UIViewController {
+class MapVC: UIViewController, CLLocationManagerDelegate {
 
+    //Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
+    //Variables
+    
+    //Constants
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        requestLocationData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func requestLocationData() {
+        let location: PrivateResource = .Location(.WhenInUse)
+        
+        proposeToAccess(location, agreed: {
+            print("I can access Location. :]\n")
+            self.locationManager.delegate = self
+            self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            self.locationManager.startUpdatingLocation()
+            self.configureMapView()
+            }, rejected: {
+                print("Location denied")
+        })
     }
-    */
-
+    
+    //MARK: - MapView Delegate Methods 
+    
+    func configureMapView(){
+        mapView.showsUserLocation = true
+    }
+    
+    
+    //MARK: - Location Manager
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let center = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+            let region = MKCoordinateRegionMake(center, MKCoordinateSpanMake(1, 1))
+            mapView.setRegion(region, animated: true)
+            locationManager.stopUpdatingLocation()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print(error.description)
+    }
+    
 }
+
+
+
