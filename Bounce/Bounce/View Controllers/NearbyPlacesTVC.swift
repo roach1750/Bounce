06e854278691b9eec1a6bca2d898b87243bec731
Andrew Roach <roach1750@gmail.com>
2016-04-02 10:03:04 -0400

    fixed rounded subtitles on distance from user

--- Bounce/Bounce/View Controllers/NearbyPlacesTVC.swift
+++ Bounce/Bounce/View Controllers/NearbyPlacesTVC.swift
@@ -13,27 +13,15 @@ class NearbyPlacesTVC: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         if let placesArray = LocationFetcher.sharedInstance.placeArray {
             return placesArray.count
         }
@@ -46,7 +34,9 @@ class NearbyPlacesTVC: UITableViewController {
         if let placesArray = LocationFetcher.sharedInstance.placeArray {
             let place = placesArray[indexPath.row]
             cell.textLabel?.text = place.name
-            cell.detailTextLabel?.text = String(place.distanceFromUser!)
+            
+            let roundedDistance = 50 * Int(round(place.distanceFromUser! / 50.0))
+            cell.detailTextLabel?.text = String(roundedDistance) + " feet away"
         }
         return cell
     }
